5301a086d61c08d548f51a901506e91383d016c7
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-04-20 10:52:33 +0530

    Update ReverseLinkedList.java

--- LinkedLists/ReverseLinkedList.java
+++ LinkedLists/ReverseLinkedList.java
@@ -5,23 +5,33 @@ import java.util.Stack;
 public class ReverseLinkedList {
     //OPTIMIZED
     public static Node ReverseList(Node head){
+        //Edge cases
        if(head == null){
         return null;
        }
+        //Edge cases
        if(head.next == null){
         return null;
        }
 
+        //pointer before a head node
        Node preNode = null;
+        //pointer at the head node
        Node currNode = head;
 
+        //begin the loop
        while(currNode != null){
+           //the third final pointer next to the current node
         Node nextNode = currNode.next;
+           //for reversing connect the current node to the previous
         currNode.next = preNode;
+           //take the previous Node to the current Node
         preNode = currNode;
+           //take the current Node forwards
         currNode = nextNode;
        }
 
+        //Move the head at the end for the final traversal to make it print the list in reverse
        head = preNode;
 
        return head;
