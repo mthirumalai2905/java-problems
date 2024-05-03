69c5e28f3f0af0004a06774905746c026c39ee3d
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-05-03 13:54:40 +0530

    Update Subset Partion.java

--- DP/Knapsack-0-1/Subset Partion.java
+++ DP/Knapsack-0-1/Subset Partion.java
@@ -1,1 +1,33 @@
+public class PartitionSubsetSum {
+    static boolean t[][] = new boolean[102][1002];
+
+    public static boolean Finder(int[] arr, int n, int sum){
+        if(sum == 0)
+          return true;
+        if(n == 0 || sum < 0)
+          return false;
+        if(t[n][sum]){
+            return true;
+        }
+        if(arr[n-1] <= sum)
+          return t[n][sum] = Finder(arr, n -1, sum - arr[n-1]) || Finder(arr, n-1, sum);
+        return t[n][sum] = Finder(arr, n-1, sum);
+    }
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 1, 2, 2, 1};
+        int n = arr.length;
+        int sum = 0;
+        if(sum % 2 != 0){
+            System.out.println("Partition into equal sum subsets is not possible");
+        } else {
+            sum /= 2;
+            boolean result = Finder(arr, n, sum);
+            if(result){
+                System.out.println("Partition into equal sum subsets is possible");
+            } else {
+                System.out.println("Partion into equal sum subsets is not possible");
+            }
+        }
+    }
+}
 
