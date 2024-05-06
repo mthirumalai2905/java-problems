3dc134e63aff5f71ff87443d1357dd18cb953c73
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-05-06 13:56:13 +0530

    Create RemoveNodeFromListWithValueGreaterThanThePresent.java

---
+++ LinkedLists/RemoveNodeFromListWithValueGreaterThanThePresent.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ListNode removeNodes(ListNode head) {
+        Stack<ListNode> stack = new Stack<>();
+        ListNode current = head;
+        
+        while (current != null) {
+            while (!stack.isEmpty() && stack.peek().val < current.val) {
+                stack.pop();
+            }
+            
+            stack.push(current);
+            current = current.next;
+        }
+        
+        ListNode dummy = new ListNode(0);
+        ListNode prev = dummy;
+        
+        for (ListNode node : stack) {
+            prev.next = node;
+            prev = prev.next;
+        }
+        
+        prev.next = null;
+        
+        return dummy.next;
+    }
+}
